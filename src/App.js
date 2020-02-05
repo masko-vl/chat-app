92009a349b50343f1091d375ca72ae4fc4cd9be6
masko-vl <58594257+masko-vl@users.noreply.github.com>
2020-02-05 10:10:25 +0100

    quest 3 completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,62 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Contact from "./components/Contact.js"
 
-function App() {
+
+class App extends Component{
+state={ users: [
+  {name:'Ana',
+   status:false,
+   avatar:'https://avatars0.githubusercontent.com/u/9919?s=280&v=4'
+   },
+   {name:'Maria',
+   status:true,
+   avatar:'https://banner2.cleanpng.com/20180326/eye/kisspng-github-computer-icons-logo-github-5ab8a338143da0.8375508315220498480829.jpg'
+   },
+   {name:'Celia',
+   status:false,
+   avatar:'https://p7.hiclipart.com/preview/191/448/847/computer-icons-github-github-logo-save-icon-format.jpg'
+   }
+
+],
+}
+changeStatus = (e) => { 
+ 
+console.log(e.target.id)
+//setstate de users en el indice qe me da e.target.id lo cambio al contrario
+/* let user = e.target.id
+let users = this.state
+let status = e.target
+console.log(this.setStatus({status:true})) */
+//this.setState({user})
+
+/* let usersCopy = JSON.parse(JSON.stringify(this.state.users))
+console.log(usersCopy);
+
+   //make changes to ingredients
+usersCopy[e.target.id].status = true//whatever new ingredients are
+console.log(usersCopy);
+ */
+console.log(this.state.users[e.target.id].status);
+
+
+//crear array del state
+let arrTemp=[...this.state.users]
+
+//si el id es el pusado cambiar el status al contrario
+arrTemp[e.target.id].status = !arrTemp[e.target.id].status
+
+//cambiar el state por el array creado
+   this.setState({
+      [this.state.users]: arrTemp
+    }) 
+    
+} 
+render() {
   return (
     <div>
-      <Contact/>
+      <Contact user={this.state.users} change={this.changeStatus}/>
     </div>
   );
 }
-
+}
 export default App;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,37 +1,21 @@
 import React from "react"
 import "./Contact.css"
 
-    const users=[
-       {name:'Ana',
-        status:true,
-        avatar:'https://avatars0.githubusercontent.com/u/9919?s=280&v=4'
-        },
-        {name:'Maria',
-        status:false,
-        avatar:'https://banner2.cleanpng.com/20180326/eye/kisspng-github-computer-icons-logo-github-5ab8a338143da0.8375508315220498480829.jpg'
-        },
-        {name:'Celia',
-        status:true,
-        avatar:'https://p7.hiclipart.com/preview/191/448/847/computer-icons-github-github-logo-save-icon-format.jpg'
-        }
-
-    ]
-
-const Contact = () => {
-    return (
-    <div>
-        {users.map(i => (
-         <div className='Contact'>
-                    <img className='avatar' src={i.avatar}></img>
-                    <div className='status'>
-                        <h4 className='name'>{i.name}</h4>
-                        <h4 className='status-text'><span className={i.status?"status-online": "status-offline"}></span>{i.status?"online": 'ofline'}</h4>
-                        
-                    </div>
-                </div>
-        ))}
-    </div>
-    )
-}
-
+    
+//i: valor x:indice
+//añadimos un id donde recogemos el indice de la persona
+const Contact =({user, change}) => (
+        <div>
+            {user.map((i, x) => (
+             <figure className='Contact' key={x}>
+                        <img className='avatar' src={i.avatar}alt=""></img>
+                        <div className='status'  >
+                            <h4 className='name' >{i.name}</h4>
+                            <h4 id={x} className='status-text' onClick={change}><span className={i.status?"status-online": "status-offline"}></span>{i.status?"online": 'ofline'}</h4>
+                            
+                        </div>
+                    </figure>
+            ))}
+        </div>
+)
 export default Contact
